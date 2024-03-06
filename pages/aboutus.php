3c82812f3b2cbb9784e9444ac205ff1b0a09e62a
wissalbn <wissal.benali88@gmail.com>
2024-03-06 17:51:46 +0100

    modif simples

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>propos-nous - Parfumerie</title>
-    <link rel="stylesheet" href="../styles/style.css">
-    
+    <link rel="stylesheet" href="../styles/styleaboutus.css">
+
 
 </head>
 
 <body>
+<div class="main">
 
- <?php include("../pages/header.php"); ?>
+
+    <?php include("../pages/header.php"); ?>
     <div class="row2">
 
         <div class="col-6">
@@ -20,14 +22,15 @@
                 <h1>À PROPOS DE NOUS</h1>
 
                 <p class=""><strong> Bienvenue chez Parfumerie Fragrancia, où l'art de la parfumerie prend vie.
-                     Nous sommes dévoués à vous offrir une expérience sensorielle unique,<br> avec une sélection 
-                     exquise de fragrances. Notre engagement envers l'excellence et le service clientèle 
-                     exceptionnel<br> fait de nous votre destination préférée pour trouver le parfum parfait.<br> 
-                     Découvrez notre collection et laissez-vous envoûter par l'essence même du luxe olfactif.<br><br>
-                </strong></p>
-               
+                        Nous sommes dévoués à vous offrir une expérience sensorielle unique,<br> avec une sélection
+                        exquise de fragrances. Notre engagement envers l'excellence et le service clientèle
+                        exceptionnel<br> fait de nous votre destination préférée pour trouver le parfum parfait.<br>
+                        Découvrez notre collection et laissez-vous envoûter par l'essence même du luxe olfactif.<br><br>
+                    </strong></p>
+
             </section>
         </div>
+    </div>
     <div class="row">
         <div class="col1">
             <br><br>
@@ -36,13 +39,14 @@
                 <img src="../images/rajae.png" class="rajae">
                 <h2>BOUFOUL Rajae</h2>
                 <label>
-                <p>
-                    <br>
-                    
-                    Je suis Rajae Boufoul, étudiante en deuxième année de DAI, âgé de 20 ans.
-                     En tant que créateur de ce site web,Parfumerie Fragrancia, je m'efforce de
-                     partager ma passion pour les fragrances et le raffinement à travers cette plateforme.</p>
-                
+                    <p>
+                        <br>
+
+                        Je suis Rajae Boufoul, étudiante en deuxième année de DAI, âgé de 20 ans.
+                        En tant que créateur de ce site web,Parfumerie Fragrancia, je m'efforce de
+                        partager ma passion pour les fragrances et le raffinement à travers cette plateforme.
+                    </p>
+
 
                 </label>
             </section>
@@ -53,16 +57,17 @@
                 <img src="../images/wissal.png" alt="wissal" class="wissalimg">
                 <h2>BENALI Wissal</h2>
                 <label>
-                <p>
-                    <br>
-                    
-                    Je suis Wissal Benali, une étudiante de 21 ans, animée par la curiosité et la passion pour
-                     l'esthétique. Actuellement en deuxième année de DAI à l'ESTO, je trouve ma voie dans
-                     la création de ce site web, Parfumerie Fragrancia.  
-                </p></label>
-                
+                    <p>
+                        <br>
 
-                
+                        Je suis Wissal Benali, une étudiante de 21 ans, animée par la curiosité et la passion pour
+                        l'esthétique. Actuellement en deuxième année de DAI à l'ESTO, je trouve ma voie dans
+                        la création de ce site web, Parfumerie Fragrancia.
+                    </p>
+                </label>
+
+
+
             </section>
         </div>
 
@@ -74,23 +79,20 @@
             <br><br>
             <h1>NOS MISSIONS</h1>
 
-            <p class=""><strong> À Parfumerie Fragrancia, notre engagement est de 
-                créer des expériences olfactives uniques, de promouvoir le 
-                raffinement et l'élégance à travers nos sélections de parfums.
-                 Nous visons à fournir un service client exceptionnel, 
-                 en répondant aux besoins de nos clients avec professionnalisme.
-                  Notre mission est également d'inspirer la passion pour la
-                   parfumerie en partageant des conseils et des informations 
-                   précieuses sur cet art captivant.<br><br>
-            </strong></p>
-           
+            <p class=""><strong> À Parfumerie Fragrancia, notre engagement est de
+                    créer des expériences olfactives uniques, de promouvoir le
+                    raffinement et l'élégance à travers nos sélections de parfums.
+                    Nous visons à fournir un service client exceptionnel,
+                    en répondant aux besoins de nos clients avec professionnalisme.
+                    Notre mission est également d'inspirer la passion pour la
+                    parfumerie en partageant des conseils et des informations
+                    précieuses sur cet art captivant.<br><br>
+                </strong></p>
+
         </section>
     </div>
-    <footer>
-
-            <?php include("../pages/footer.php"); ?>
-        
-    </footer>
+    </div>
+    <footer><?php include('../pages/footer.php'); ?></footer>
 
 </body>
 

fe39b8d3e883437574eeed9fcfe06961b430b50f
wissalbn <wissal.benali88@gmail.com>
2024-03-07 03:25:16 +0100

    modif css

--- pages/return.php
+++ pages/return.php
@@ -5,24 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Retourner Produit</title>
-    <link rel="stylesheet" href="../styles/styleretourpoduit.css">
+    <link rel="stylesheet" href="../styles/stylereturn.css">
 </head>
 
 <body>
-    <div class="row">
-        <?php include("../pages/header.php"); ?>
-    </div>
+    <?php include("../pages/header.php"); ?>
     <div class="main">
-        <h1>RETOURNER UN PRODUIT</h1>
-        <p><strong>Vous avez reçu un produit abimé ou qui ne correspond pas à votre commande ?</strong></p>
-        <p><strong>Nous attribuons un grand soin à la préparation et à l’emballage de vos commandes, donc cela ne peut arriver que très rarement. Dans ce cas, vous pouvez nous retourner le produit en respectant le délai de 7 jours et sans oublier le bon de livraison. Les frais de retour du colis et de la deuxième livraison en cas d’un échange seront à la charge de Fragrancia. Si votre colis a subi des dommages remettant en question ses caractéristiques pendant le transport, merci d’en refuser la réception et d’en informer notre service clients.</strong></p>
-        <p><strong>Vous avez acheté un produit sur Fragrancia, mais vous avez changé d’avis et vous souhaitez le retourner en vue d’un échange ou d’un remboursement ? Vous disposez d’un délai de 7 jours à partir de la date de réception de votre colis pour exercer votre droit de rétractation si vous respectez les conditions suivantes :
-                Le produit doit être non ouvert, non abimé et son emballage d’origine doit être non descellé.
-                Vous devez nous renvoyer le colis avec le bon de livraison.
-                Les frais de livraison et de retour restent à votre charge.</strong></p>
-        <p><strong>Si vous souhaitez échanger votre produit, veuillez prendre contact avec notre service clients par e-mail ou téléphone pour indiquer le nouveau produit que vous souhaitez.</strong></p>
-        <p><strong>Si vous souhaitez un remboursement, et dans le cas où les frais de la livraison étaient offerts, le montant remboursé correspondra au montant de la commande diminué des frais de la livraison.</strong></p>
-        <p><strong>Afin de garantir la qualité et l'authenticité des produits en vente sur Fragrancia, le retour de certains produits n'est pas autorisé.</strong></p>
+
+        <div class="title">
+            <h1>RETOURNER UN PRODUIT</h1>
+        </div>
+        <div class="container-fluid para">
+            Vous avez reçu un produit abimé ou qui ne correspond pas à votre commande ?<br><br>
+
+            Nous attribuons un grand soin à la préparation et à l’emballage de vos commandes, donc cela ne peut arriver que très rarement. Dans ce cas, vous pouvez nous retourner le produit en respectant le délai de 7 jours et sans oublier le bon de livraison. Les frais de retour du colis et de la deuxième livraison en cas d’un échange seront à la charge de Fragrancia. Si votre colis a subi des dommages remettant en question ses caractéristiques pendant le transport, merci d’en refuser la réception et d’en informer notre service clients.<br><br>
+
+            Vous avez acheté un produit sur Fragrancia, mais vous avez changé d’avis et vous souhaitez le retourner en vue d’un échange ou d’un remboursement ? Vous disposez d’un délai de 7 jours à partir de la date de réception de votre colis pour exercer votre droit de rétractation si vous respectez les conditions suivantes : <br>
+            <ul class="list">
+                <li>Le produit doit être non ouvert, non abimé et son emballage d’origine doit être non descellé.</li>
+                <li>Vous devez nous renvoyer le colis avec le bon de livraison.</li>
+                <li>Les frais de livraison et de retour restent à votre charge.</li>
+            </ul><br>
+            Si vous souhaitez échanger votre produit, veuillez prendre contact avec notre service clients par e-mail ou téléphone pour indiquer le nouveau produit que vous souhaitez. <br>
+
+            Si vous souhaitez un remboursement, et dans le cas où les frais de la livraison étaient offerts, le montant remboursé correspondra au montant de la commande diminué des frais de la livraison. <br>
+
+            Afin de garantir la qualité et l'authenticité des produits en vente sur Fragrancia, le retour de certains produits n'est pas autorisé. <br>
+        </div>
     </div>
     <footer>
         <?php include("../pages/footer.php") ?>

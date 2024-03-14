66d4bb2f98a6ea08199b4382e494b2dabe10f457
wissalbn <wissal.benali88@gmail.com>
2024-03-14 22:37:56 +0000

    login

--- pages/login.php
+++ pages/login.php
@@ -1,26 +1,42 @@
 <?php
 session_start();
-$dbb = new PDO('mysql:host=localhost;dbname=parfumerie;charset=utf8;', 'root', '');
+require_once('../pages/connection.php');
+if (isset($_POST['connexion'])) {
+    if (!empty($_POST['emailclient']) and !empty($_POST['mdpclient'])) {
+        $emailclient = htmlspecialchars($_POST['emailclient']);
+        $mdpclient = $_POST['mdpclient'];
 
-if(isset($_POST['connexion'])){
-    // Vérifier si les champs sont remplis
-    if(!empty($_POST['emailclient']) AND !empty($_POST['mdpclient']) ){
-        $emailclient= htmlspecialchars($_POST['emailclient']);
-        $mdpclient= $_POST['mdpclient'];
-
-        // Récupérer le mot de passe haché de la base de données
-        $recupUser = $dbb->prepare('SELECT * FROM utilisateur WHERE emailclient= ?');
+        $recupUser = $bdd->prepare('SELECT * FROM utilisateur WHERE EMAILCLIENT= ?');
         $recupUser->execute(array($emailclient));
         $user = $recupUser->fetch();
-        if($user && password_verify($mdpclient, $user['MDPCLIENT'])){
+        if ($user && password_verify($mdpclient, $user['MDPCLIENT'])) {
             $_SESSION['emailclient'] = $emailclient;
             header('Location: index.php');
-            exit; 
+            exit;
         } else {
+
             $error_message = "*Email ou mot de passe est incorrect !";
         }
     } else {
         $error_message = "*Veuillez compléter tous les champs...";
+    }
+
+    $session_timeout = 7200; // 2 hours * 60 minutes * 60 seconds
+
+    if (isset($_SESSION['last_activity']) && isset($_SESSION['emailclient'])) {
+        $elapsed_time = time() - $_SESSION['last_activity'];
+
+        if ($elapsed_time > $session_timeout) {
+            session_unset();
+            session_destroy();
+            header('Location: login.php');
+            exit;
+        } else {
+            $_SESSION['last_activity'] = time();
+        }
+    } else {
+        header('Location: login.php');
+        exit;
     }
 }
 ?>
@@ -34,7 +50,7 @@ if(isset($_POST['connexion'])){
     <title>se connecter - Parfumerie</title>
     <link rel="stylesheet" href="../styles/styleconnecter.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+
 </head>
 
 <body>
@@ -43,11 +59,11 @@ if(isset($_POST['connexion'])){
         <div class="all row mt-1 main2">
             <div class="row">
                 <div class="col-md-6 col-img mt-0">
-                    <div >
+                    <div>
                         <img src="../images/inscrire_connect.png" class="img-fluid">
                     </div>
                 </div>
-            
+
                 <div class="col-md-6 col">
                     <div class="c1">
                         <h1>BON RETOUR!</h1>
@@ -56,37 +72,37 @@ if(isset($_POST['connexion'])){
                             <div class="formulaire">
                                 <input type="text" name="emailclient" placeholder="EMAIL" autocomplete="off"><br>
                                 <div style="position: relative;">
-                                <input type="password" name="mdpclient" placeholder="MOT DE PASSE*" autocomplete="off" id="password-input">
-                                <span class="password-toggle" id="password-toggle">&#x1f441;</span>
-                            </div><br>
+                                    <input type="password" name="mdpclient" placeholder="MOT DE PASSE*" autocomplete="off" id="password-input">
+                                    <span class="password-toggle" id="password-toggle">&#x1f441;</span>
+                                </div><br>
                                 <button type="submit" name="connexion" class="btnc">CONNEXION</button>
                             </div>
                         </form>
                         <?php
-                        
-                        if(isset($error_message)){
+
+                        if (isset($error_message)) {
                             echo '<span class="error-message">' . $error_message . '</span>';
                         }
                         ?>
                     </div>
-                
+
+                </div>
             </div>
         </div>
-    </div>
 
-    <footer>
-        <?php include("../pages/footer.php"); ?>
-    </footer>
-    <script>
-document.getElementById("password-toggle").addEventListener("click", function() {
-    var passwordInput = document.getElementById("password-input");
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
-});
-</script>
+        <footer>
+            <?php include("../pages/footer.php"); ?>
+        </footer>
+        <script>
+            document.getElementById("password-toggle").addEventListener("click", function() {
+                var passwordInput = document.getElementById("password-input");
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                } else {
+                    passwordInput.type = "password";
+                }
+            });
+        </script>
 </body>
 
 </html>

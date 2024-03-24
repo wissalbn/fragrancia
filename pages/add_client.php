f37c6d0a25272af1a8ac4535c422251b6ed061ae
rajaeBf <rajae.boufoul22@ump.ac.ma>
2024-03-24 17:45:10 +0000

    ch

--- pages/add_client.php
+++ pages/add_client.php
@@ -5,7 +5,6 @@ include("connection.php");
 // Définir des variables pour les valeurs par défaut du formulaire
 $id_client = $nom = $civilite = $email = $telephone = $pays = $ville = $adresse = "";
 
-// Vérifier si le formulaire est soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs du formulaire
     $nom = $_POST["nom"];
@@ -15,17 +14,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pays = $_POST["pays"];
     $ville = $_POST["ville"];
     $adresse = $_POST["adresse"];
+    $mdp = $_POST["mdp"];
+
+    // Hasher le mot de passe
+    $hashed_mdp = password_hash($mdp, PASSWORD_DEFAULT);
 
     // Requête SQL pour insérer le nouveau client dans la base de données
-    $sql_insert_client = "INSERT INTO utilisateur (NOMCLIENT, CIVILITECLIENT, EMAILCLIENT, TELCLIENT, PAYS, VILLECLIENT, ADRESSECLIENT) VALUES ('$nom', '$civilite', '$email', '$telephone', '$pays', '$ville', '$adresse')";
+    $sql_insert_client = "INSERT INTO utilisateur (NOMCLIENT, CIVILITECLIENT, EMAILCLIENT, TELCLIENT, PAYS, VILLECLIENT, ADRESSECLIENT, MDPCLIENT) VALUES ('$nom', '$civilite', '$email', '$telephone', '$pays', '$ville', '$adresse', '$hashed_mdp')";
 
     // Exécuter la requête SQL
     if (mysqli_query($bdd, $sql_insert_client)) {
         // Rediriger vers une page de succès ou afficher un message de succès
-        header("Location: adminPanel.php#client");
+        header("Location: client.php");
         exit;
     } else {
         echo "Erreur lors de l'ajout du client: " . mysqli_error($bdd);
     }
 }
+
 ?>
